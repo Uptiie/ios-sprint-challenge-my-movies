279888dc33a41e57ac00dc2cd1a900548e9e9eef
Donovan Langager <donovan.lan16@gmail.com>
2019-09-29 11:39:04 -0700

    Added Core Data Stack

---
+++ MyMovies/Models/New Group/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  MyMovies
+//
+//  Created by Uptiie on 9/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataStack {
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                NSLog("Unable to save context: \(error)")
+                context.reset()
+            }
+        }
+    }
+    
+    static let shared = CoreDataStack()
+    
+    let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "MovieCoreData" as String)
+        container.loadPersistentStores() { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext { return container.viewContext }
+}
